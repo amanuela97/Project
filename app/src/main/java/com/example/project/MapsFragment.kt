20d7781bc82ad749df32496e8ec915d5d3d52224
amanuela97 <babyzewdie@gmail.com>
2020-09-30 19:17:36 +0300

    functional

--- app/src/main/java/com/example/project/MapsFragment.kt
+++ app/src/main/java/com/example/project/MapsFragment.kt
@@ -23,7 +23,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
